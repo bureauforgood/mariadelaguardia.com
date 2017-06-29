7849c0256e3ff001746b44f4ae63034317117d89
Paul Chamberlain <pchamberlain@bluestatedigital.com>
2017-06-29 19:04:37 -0400

    Make html-header include

--- gulpfile.js
+++ gulpfile.js
@@ -9,25 +9,25 @@
 // PLUGINS
 // --------
 var autoprefixer = require('gulp-autoprefixer'),
-    browserSync = require('browser-sync').create(),
-    cssnano = require('gulp-cssnano'),
-    gulp = require('gulp'),
-    notify = require('gulp-notify'),
-    p = require('./package.json'),
-    pug = require('gulp-pug'),
-    sass = require('gulp-sass');
+  browserSync = require('browser-sync').create(),
+  cssnano = require('gulp-cssnano'),
+  gulp = require('gulp'),
+  notify = require('gulp-notify'),
+  p = require('./package.json'),
+  pug = require('gulp-pug'),
+  sass = require('gulp-sass');
 
 
 // VARIABLES
 // ----------
 var dist = 'dist/',
-    source = 'src/';
+  source = 'src/';
 
 
 // ERROR HANDLING
 // ---------------
 function handleError() {
-    this.emit('end');
+  this.emit('end');
 }
 
 // BUILD SUBTASKS
@@ -35,33 +35,34 @@ function handleError() {
 
 // Styles
 gulp.task('styles', function() {
-    return gulp.src([
-        source+'scss/style.scss'
-    ])
-    .pipe(sass()
-        .on('error', handleError)
-        .on('error', notify.onError()))
-    .pipe(autoprefixer())
+  return gulp.src([
+    source+'scss/style.scss'
+  ])
+  .pipe(sass()
+    .on('error', handleError)
+    .on('error', notify.onError()))
+  .pipe(autoprefixer())
 //    .pipe(cssnano())
-    .pipe(gulp.dest(dist+'css/'))
-    .pipe(browserSync.stream());
+  .pipe(gulp.dest(dist+'css/'))
+  .pipe(browserSync.stream());
 });
 
 gulp.task('pug',function() {
-    return gulp.src(source+'views/*.pug')
-    .pipe(pug({
-        doctype: 'html',
-        pretty: true
-    }))
-    .pipe(gulp.dest(dist))
-    .pipe(browserSync.stream());
+  return gulp.src(source+'views/*.pug')
+  .pipe(pug({
+    doctype: 'html',
+    basedir: source,
+    pretty: true
+  }))
+  .pipe(gulp.dest(dist))
+  .pipe(browserSync.stream());
 });
 
 // Copy Images
 gulp.task('copy', function() {
-    gulp.src([source+'img/**/*'])
-    .pipe(gulp.dest(dist+'img'))
-    .pipe(browserSync.stream());
+  gulp.src([source+'img/**/*'])
+  .pipe(gulp.dest(dist+'img'))
+  .pipe(browserSync.stream());
 });
 
 // BUILD TASKS
@@ -69,21 +70,21 @@ gulp.task('copy', function() {
 
 gulp.task('default', function() {
 
-    gulp.start('styles', 'pug', 'copy');
+  gulp.start('styles', 'pug', 'copy');
 
-    browserSync.init({
-        server: {
-            baseDir: "./dist/"
-        }
-    });
+  browserSync.init({
+    server: {
+      baseDir: "./dist/"
+    }
+  });
 
-    // Watch .scss files
-    gulp.watch(source+'scss/**/*.scss', ['styles']);
+  // Watch .scss files
+  gulp.watch(source+'scss/**/*.scss', ['styles']);
 
-    // Watch index file
-    gulp.watch(source+'views/**/*.pug', ['pug']);
+  // Watch index file
+  gulp.watch(source+'views/**/*.pug', ['pug']);
 
-    // Watch images directory
-    gulp.watch(source+'img/**/*', ['copy']);
+  // Watch images directory
+  gulp.watch(source+'img/**/*', ['copy']);
 
 });
